01358a1d59be41efd936bf52d6f9af5380711f1a
luotao <luotaoyeah@gmail.com>
2019-09-14 19:36:54 +0800

    feat(ui.tutorial): step-3-implement-the-button-row

--- lib/doc/02_development/01_ui/02_layout/02_tutorial/02_01_02_02.dart
+++ lib/doc/02_development/01_ui/02_layout/02_tutorial/02_01_02_02.dart
@@ -4,51 +4,83 @@ import 'package:flutter/material.dart';
 class X02010202 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget _titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    "Oeschinen Lake Campground",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  "Kandersteg, Switzerland",
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Container(
+            child: Text("41"),
+            padding: const EdgeInsets.only(left: 3),
+          )
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget _buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.phone, "CALL"),
+        _buildButtonColumn(color, Icons.near_me, "ROUTE"),
+        _buildButtonColumn(color, Icons.share, "SHARE"),
+      ],
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Tutorial", style: TextStyle(letterSpacing: -0.5)),
       ),
       body: Column(
-        children: <Widget>[_titleSection],
+        children: <Widget>[_titleSection, _buttonSection],
       ),
     );
   }
 
-  final Widget _titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
+  Widget _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  "Oeschinen Lake Campground",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Text(
-                "Kandersteg, Switzerland",
-                style: TextStyle(
-                  color: Colors.grey[500],
-                ),
-              )
-            ],
-          ),
-        ),
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
+        Icon(icon, color: color),
         Container(
-          child: Text("41"),
-          padding: const EdgeInsets.only(left: 3),
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
         )
       ],
-    ),
-  );
+    );
+  }
 }
