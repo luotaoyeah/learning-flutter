0e2462edd45b23002268567fbdfa6b03156b675f
luotao <luotaoyeah@gmail.com>
2019-09-14 18:50:19 +0800

    feat(ui.tutorial): step-0-create-the-app-base-code

--- lib/doc/02_development/01_ui/02_layout/02_01_02.dart
+++ lib/doc/02_development/01_ui/02_layout/02_01_02.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:learning_flutter/common/app_menu.dart';
 import 'package:learning_flutter/doc/02_development/01_ui/02_layout/01_layouts_in_flutter/02_01_02_01.dart';
+
+import '02_tutorial/02_01_02_02.dart';
 
 /// https://flutter.dev/docs/development/ui/widgets-intro
 class X020102 extends StatelessWidget {
+  final List<AppMenu> _menus = [
+    AppMenu(title: "Layouts In Flutter", widget: X02010201()),
+    AppMenu(title: "Tutorial", widget: X02010202()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,18 +19,14 @@ class X020102 extends StatelessWidget {
       ),
       body: ListView(
         children: ListTile.divideTiles(
-          tiles: [
-            ListTile(
-              title: Text("Layouts In Flutter", style: TextStyle(letterSpacing: -0.5)),
-              onTap: () {
-                Navigator.of(context).push(
-                  PageRouteBuilder(
-                    pageBuilder: (x, y, z) => X02010201(),
-                  ),
-                );
-              },
-            ),
-          ],
+          tiles: _menus.map((m) => ListTile(
+                title: Text(m.title, style: TextStyle(letterSpacing: -0.5)),
+                onTap: () {
+                  Navigator.of(context).push(
+                    PageRouteBuilder(pageBuilder: (x, y, z) => m.widget),
+                  );
+                },
+              )),
           context: context,
         ).toList(),
       ),
---
+++ lib/doc/02_development/01_ui/02_layout/02_tutorial/02_01_02_02.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// https://flutter.dev/docs/development/ui/layout/tutorial
+class X02010202 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tutorial", style: TextStyle(letterSpacing: -0.5)),
+      ),
+      body: Center(
+        child: Text("TODO"),
+      ),
+    );
+  }
+}
