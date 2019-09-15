b51df5f974d9702f86a7e670d7355846cf126bb0
luotao <luotaoyeah@gmail.com>
2019-09-15 17:30:07 +0800

    feat(ui.interactive): managing-state

--- lib/doc/02_development/01_ui/04_adding_interactivity/02_01_04.dart
+++ lib/doc/02_development/01_ui/04_adding_interactivity/02_01_04.dart
@@ -1,102 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:learning_flutter/common/app_menu.dart';
 
-import 'favorite_widget.dart';
+import '01_creating_a_stateful_widget/02_01_04.01.dart';
+import '02_managing_state/02_01_04.02.dart';
 
 /// https://flutter.dev/docs/development/ui/interactive
 class X020104 extends StatelessWidget {
+  final List<AppMenu> _menus = [
+    AppMenu(title: "Creating a stateful widget", widget: X02010401()),
+    AppMenu(title: "Managing State", widget: X02010402()),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    Widget _titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    "Oeschinen Lake Campground",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  "Kandersteg, Switzerland",
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                )
-              ],
-            ),
-          ),
-          FavoriteWidget()
-        ],
-      ),
-    );
-
-    Color color = Theme.of(context).primaryColor;
-
-    Widget _buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.phone, "CALL"),
-        _buildButtonColumn(color, Icons.near_me, "ROUTE"),
-        _buildButtonColumn(color, Icons.share, "SHARE"),
-      ],
-    );
-
-    Widget _textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Adding Interactivity", style: TextStyle(letterSpacing: -0.5)),
       ),
       body: ListView(
-        children: <Widget>[
-          Image.asset(
-            "assets/lake.jpg",
-            fit: BoxFit.cover,
-          ),
-          _titleSection,
-          _buttonSection,
-          _textSection,
-        ],
-      ),
-    );
-  }
-
-  Widget _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
+        children: ListTile.divideTiles(
+          tiles: _menus.map(
+            (menu) => ListTile(
+              title: Text(
+                menu.title,
+                style: TextStyle(letterSpacing: -0.5),
+              ),
+              onTap: () {
+                Navigator.of(context).push(
+                  PageRouteBuilder(pageBuilder: (x, y, z) => menu.widget),
+                );
+              },
             ),
           ),
-        )
-      ],
+          context: context,
+        ).toList(),
+      ),
     );
   }
 }
---
+++ lib/doc/02_development/01_ui/04_adding_interactivity/02_managing_state/02_01_04.02.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// https://flutter.dev/docs/development/ui/interactive#managing-state
+class X02010402 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Managing State", style: TextStyle(letterSpacing: -0.5)),
+      ),
+      body: Text("TODO"),
+    );
+  }
+}
