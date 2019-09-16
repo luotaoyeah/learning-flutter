826bf8e25798c1ebbf7cfc5b412e3204c94ec19b
luotao <luotaoyeah@gmail.com>
2019-09-16 19:00:13 +0800

    feat(ui.assets-and-images): DefaultAssetBundle.of(context)

--- lib/doc/02_development/01_ui/05_assets_and_images/01_loading_text_assets/02_01_05.01.dart
+++ lib/doc/02_development/01_ui/05_assets_and_images/01_loading_text_assets/02_01_05.01.dart
@@ -9,9 +9,19 @@ class X02010501 extends StatefulWidget {
 
 class _X02010501State extends State<X02010501> {
   String _json = "";
+  String _json02 = "";
 
   @override
   Widget build(BuildContext context) {
+    /// 如果可以拿到 `context`, 则推荐使用 `DefaultAssetBundle.of(context)`
+    void _loadJSON02() {
+      DefaultAssetBundle.of(context).loadString("assets/files/02_01_05.01.json").then((data) {
+        setState(() {
+          _json02 = data;
+        });
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Loading Text Assets", style: TextStyle(letterSpacing: -0.5)),
@@ -20,7 +30,7 @@ class _X02010501State extends State<X02010501> {
         padding: const EdgeInsets.all(10),
         children: <Widget>[
           RaisedButton(
-            child: Text("加载"),
+            child: Text("加载 [ 使用 rootBundle ]"),
             onPressed: _loadJSON,
           ),
           Container(
@@ -33,12 +43,29 @@ class _X02010501State extends State<X02010501> {
               border: Border.all(width: 1),
               borderRadius: BorderRadius.all(Radius.circular(3)),
             ),
-          )
+          ),
+          Divider(),
+          RaisedButton(
+            child: Text("加载 [ 使用 DefaultAssetBundle.of(context) ]"),
+            onPressed: _loadJSON02,
+          ),
+          Container(
+            child: Text(
+              _json02,
+              style: TextStyle(fontFamily: "courier new"),
+            ),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(width: 1),
+              borderRadius: BorderRadius.all(Radius.circular(3)),
+            ),
+          ),
         ],
       ),
     );
   }
 
+  /// 如果无法拿到 `context`, 就可以使用 `rootBundle`
   void _loadJSON() {
     rootBundle.loadString("assets/files/02_01_05.01.json").then((data) {
       setState(() {
