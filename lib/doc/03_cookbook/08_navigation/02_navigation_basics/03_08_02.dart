176d26bc73fcfd80ed6e03e502b3618c9b851604
luotao <luotaoyeah@gmail.com>
2019-09-17 12:56:14 +0800

    feat(ui.assets-and-images): navigate to a new screen and back

--- lib/doc/03_cookbook/08_navigation/02_navigation_basics/03_08_02.dart
+++ lib/doc/03_cookbook/08_navigation/02_navigation_basics/03_08_02.dart
@@ -8,7 +8,45 @@ class X030802 extends StatelessWidget {
       appBar: AppBar(
         title: Text("Navigate To A New Screen And Back", style: TextStyle(letterSpacing: -0.5)),
       ),
-      body: Text("TODO"),
+      body: Center(
+        child: RaisedButton(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text("GOTO NEXT ROUTE"),
+              Icon(Icons.arrow_right),
+            ],
+          ),
+          onPressed: () {
+            Navigator.push(context, PageRouteBuilder(pageBuilder: (x, y, z) => _SecondRoute()));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route", style: TextStyle(letterSpacing: -0.5)),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(Icons.arrow_left),
+              Text("GOTO BACK"),
+            ],
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
     );
   }
 }
