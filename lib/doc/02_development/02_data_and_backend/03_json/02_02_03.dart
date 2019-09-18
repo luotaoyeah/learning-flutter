a701bc572b6296e9fe64495c73b4eec9bd368c77
luotao <luotaoyeah@gmail.com>
2019-09-18 18:11:04 +0800

    feat(ui.development.data-and-backend): json

---
+++ lib/doc/02_development/02_data_and_backend/03_json/02_02_03.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// https://flutter.dev/docs/development/data-and-backend/json
+class X020203 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('JSON And Serialization', style: TextStyle(letterSpacing: -0.5)),
+      ),
+      body: Text("TODO"),
+    );
+  }
+}
