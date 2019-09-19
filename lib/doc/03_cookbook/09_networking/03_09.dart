c56a3bdeeefc74a521b318a90db1e0f5b56495c1
luotao <luotaoyeah@gmail.com>
2019-09-19 16:55:09 +0800

    feat(ui.cookbook.networking): background-parsing

--- lib/doc/03_cookbook/09_networking/03_09.dart
+++ lib/doc/03_cookbook/09_networking/03_09.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:learning_flutter/common/app_menu.dart';
 
 import '01_fetch_data/03_09_01.dart';
+import '03_background_parsing/03_09_03.dart';
 
 /// https://flutter.dev/docs/cookbook#networking
 class X0309 extends StatelessWidget {
   final List<AppMenu> _menus = [
     AppMenu(title: "Fetch Data From The Internet", widget: X030901()),
+    AppMenu(title: "Parse JSON In The Background", widget: X030903()),
   ];
 
   @override
---
+++ lib/doc/03_cookbook/09_networking/03_background_parsing/03_09_03.dart
@@ -0,0 +1,97 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:learning_flutter/common/article.dart';
+import 'package:learning_flutter/common/page.dart';
+import 'package:learning_flutter/common/result.dart';
+
+/// https://flutter.dev/docs/cookbook/networking/background-parsing
+class X030903 extends StatefulWidget {
+  @override
+  _X030903State createState() => _X030903State();
+}
+
+class _X030903State extends State<X030903> {
+  Future<Page<Article>> pageFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    this.pageFuture = this._fetchArticles();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Parse JSON In The Background", style: TextStyle(letterSpacing: -0.5)),
+      ),
+      body: Center(
+        child: FutureBuilder<Page<Article>>(
+          future: pageFuture,
+          builder: (BuildContext context, AsyncSnapshot<Page<Article>> snapshot) {
+            if (snapshot.hasData) {
+              return _buildArticleList(snapshot.data, context);
+            } else if (snapshot.hasError) {
+              return Text(snapshot.error);
+            }
+
+            return CircularProgressIndicator(
+              strokeWidth: 1,
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildArticleList(Page<Article> page, BuildContext context) {
+    return ListView(
+      children: ListTile.divideTiles(
+        tiles: page.data.map(
+          (article) => Container(
+            child: ListTile(
+              leading: Image.network(
+                "http://192.168.1.14:17202/api/picture/file/thumb?fileName=${article.cover.fileName}",
+                fit: BoxFit.fill,
+              ),
+              title: Text(article.title),
+            ),
+          ),
+        ),
+        context: context,
+      ).toList(),
+    );
+  }
+
+  Future<Page<Article>> _fetchArticles() async {
+    var response = await http.post("http://192.168.1.14:17202/api/article", body: {});
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return compute(_parsePage, response.body);
+    } else {
+      throw Exception("fail to fetch articles");
+    }
+  }
+}
+
+/// [compute] 的第一个参数, 必须是一个底层函数,
+/// 不能是一个 instance method 或者 static method
+Page<Article> _parsePage(String body) {
+  Result<dynamic> _result = Result<dynamic>.fromJson(json.decode(body));
+
+  if (!_result.status) {
+    throw Exception(_result.message);
+  } else {
+    Page<dynamic> _page = Page<dynamic>.fromJson(_result.data);
+
+    Page<Article> page = Page<Article>();
+    page.totalCount = _page.totalCount;
+    page.data = _page.data.map((i) => Article.fromJson(i)).toList(growable: false);
+
+    return page;
+  }
+}
