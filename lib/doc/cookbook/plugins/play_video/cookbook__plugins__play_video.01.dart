e66159e5e0b9f26a0ecbf1446f967404bfb39532
luotao <luotaoyeah@gmail.com>
2019-10-08 18:39:18 +0800

    feat(cookbook.plugins): play-video

--- lib/doc/cookbook/plugins/play_video/cookbook__plugins__play_video.01.dart
+++ lib/doc/cookbook/plugins/play_video/cookbook__plugins__play_video.01.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:learning_flutter/common/article.dart';
+import 'package:video_player/video_player.dart';
 
+///
+/// 错误:    Cleartext HTTP traffic to 192.168.1.16 not permitted
+/// 解决方法: https://medium.com/@imstudio/android-8-cleartext-http-traffic-not-permitted-73c1c9e3b803
+///
 class CookbookPluginsPlayVideo01 extends StatefulWidget {
   final Article article;
 
@@ -11,9 +16,23 @@ class CookbookPluginsPlayVideo01 extends StatefulWidget {
 }
 
 class _CookbookPluginsPlayVideo01State extends State<CookbookPluginsPlayVideo01> {
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
   @override
   void initState() {
+    _controller =
+        VideoPlayerController.network("http://192.168.1.16:17202/api/video/file?id=${widget.article.videos[0].id}");
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -22,7 +41,35 @@ class _CookbookPluginsPlayVideo01State extends State<CookbookPluginsPlayVideo01>
       appBar: AppBar(
         title: Text(widget.article.title, style: TextStyle(letterSpacing: -0.5)),
       ),
-      body: Text("TODO"),
+      body: FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(strokeWidth: 1),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              _controller.play();
+            }
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
     );
   }
 }
